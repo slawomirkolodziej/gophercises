0ff3d12225314a80be5beb399b481d2e6601e774
Sławomir Kołodziej <slawomir.kolodziej@netguru.pl>
2020-06-28 19:15:13 +0200

    complete url shortener

--- urlshortener/urlshort/urlshort.go
+++ urlshortener/urlshort/urlshort.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -12,12 +14,21 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	handler := func(writer http.ResponseWriter, request *http.Request) {
-		http.Redirect(writer, request, "http://google.com", 301)
-
-		// fallback.ServeHTTP(writer, request)
+		url := request.URL
+		redirectURL, found := pathsToUrls[url.Path]
+		if found {
+			http.Redirect(writer, request, redirectURL, 302)
+		} else {
+			fallback.ServeHTTP(writer, request)
+		}
 	}
 
 	return handler
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -36,7 +47,28 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := buildMap(pathUrls)
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
 }
